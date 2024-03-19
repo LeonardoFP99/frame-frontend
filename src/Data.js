5a48c430cd99fc6e8f34c3711bac2d8b9b6ed2a3
Leonardo Flor <leonardoflordepaula@gmail.com>
2024-03-18 22:02:03 -0300

    first commit

---
+++ src/Data.js
@@ -0,0 +1,72 @@
+export const articles = [
+    {
+        title: "Articulus 1",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eget ligula eu lectus lobortis condimentum. Aliquam nonummy auctor massa. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nulla at risus. Quisque purus magna, auctor et, sagittis ac, posuere eu, lectus. Nam mattis, felis ut adipiscing.",
+        image: "https://www.oficinadanet.com.br/media/post/25197/330/images.jpg",
+        likes: 5,
+        comments: 4
+    },
+    {
+        title: "Articulus 2",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eget ligula eu lectus lobortis condimentum. Aliquam nonummy auctor massa. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nulla at risus. Quisque purus magna, auctor et, sagittis ac, posuere eu, lectus. Nam mattis, felis ut adipiscing.",
+        image: "https://www.oficinadanet.com.br/media/post/25197/330/images.jpg",
+        likes: 5,
+        comments: 4
+    },
+    {
+        title: "Articulus 3",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eget ligula eu lectus lobortis condimentum. Aliquam nonummy auctor massa. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nulla at risus. Quisque purus magna, auctor et, sagittis ac, posuere eu, lectus. Nam mattis, felis ut adipiscing.",
+        image: "https://www.oficinadanet.com.br/media/post/25197/330/images.jpg",
+        likes: 5,
+        comments: 4
+    },
+    {
+        title: "Articulus 4",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eget ligula eu lectus lobortis condimentum. Aliquam nonummy auctor massa. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nulla at risus. Quisque purus magna, auctor et, sagittis ac, posuere eu, lectus. Nam mattis, felis ut adipiscing.",
+        image: "https://www.oficinadanet.com.br/media/post/25197/330/images.jpg",
+        likes: 5,
+        comments: 4
+    },
+    {
+        title: "Articulus 5",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eget ligula eu lectus lobortis condimentum. Aliquam nonummy auctor massa. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nulla at risus. Quisque purus magna, auctor et, sagittis ac, posuere eu, lectus. Nam mattis, felis ut adipiscing.",
+        image: "https://www.oficinadanet.com.br/media/post/25197/330/images.jpg",
+        likes: 5,
+        comments: 4
+    },
+    {
+        title: "Articulus 6",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eget ligula eu lectus lobortis condimentum. Aliquam nonummy auctor massa. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nulla at risus. Quisque purus magna, auctor et, sagittis ac, posuere eu, lectus. Nam mattis, felis ut adipiscing.",
+        image: "https://www.oficinadanet.com.br/media/post/25197/330/images.jpg",
+        likes: 5,
+        comments: 4
+    },
+    {
+        title: "Articulus 7",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eget ligula eu lectus lobortis condimentum. Aliquam nonummy auctor massa. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nulla at risus. Quisque purus magna, auctor et, sagittis ac, posuere eu, lectus. Nam mattis, felis ut adipiscing.",
+        image: "https://www.oficinadanet.com.br/media/post/25197/330/images.jpg",
+        likes: 5,
+        comments: 4
+    },
+    {
+        title: "Articulus 8",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eget ligula eu lectus lobortis condimentum. Aliquam nonummy auctor massa. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nulla at risus. Quisque purus magna, auctor et, sagittis ac, posuere eu, lectus. Nam mattis, felis ut adipiscing.",
+        image: "https://www.oficinadanet.com.br/media/post/25197/330/images.jpg",
+        likes: 5,
+        comments: 4
+    },
+    {
+        title: "Articulus 9",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eget ligula eu lectus lobortis condimentum. Aliquam nonummy auctor massa. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nulla at risus. Quisque purus magna, auctor et, sagittis ac, posuere eu, lectus. Nam mattis, felis ut adipiscing.",
+        image: "https://www.oficinadanet.com.br/media/post/25197/330/images.jpg",
+        likes: 5,
+        comments: 4
+    },
+    {
+        title: "Articulus 10",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eget ligula eu lectus lobortis condimentum. Aliquam nonummy auctor massa. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nulla at risus. Quisque purus magna, auctor et, sagittis ac, posuere eu, lectus. Nam mattis, felis ut adipiscing.",
+        image: "https://www.oficinadanet.com.br/media/post/25197/330/images.jpg",
+        likes: 5,
+        comments: 4
+    }
+];
